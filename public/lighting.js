e6ca025ef48cc730c433795b5b559cd3024a878a
hswick <hswick@smu.edu>
2016-09-29 21:00:31 -0500

    Moved lighting to a different file

---
+++ public/lighting.js
@@ -0,0 +1,26 @@
+
+
+function lighting(w, h, scene) {
+
+var ambient = new THREE.AmbientLight( 0xffffff, 0.7 );
+
+var spotlight = new THREE.SpotLight(new THREE.Color(1, 1, 1));
+
+spotlight.position.set(0, 200, 600);
+spotlight.castShadow = true;
+spotlight.angle = 0.5
+spotlight.intensity = 1;
+spotlight.penumbra = 0.5;
+spotlight.distance = w * 2;
+spotlight.decay = 3;
+
+spotlight.shadow.mapSize.width = 2048 + 1024;
+spotlight.shadow.mapSize.height = 2048 + 1024;
+
+//spotlight & ambient received from lighting.js
+scene.add(spotlight, ambient);
+
+var sphelper = new THREE.SpotLightHelper(spotlight);
+scene.add(sphelper);
+
+}
