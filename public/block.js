140e8956bdc00bb5846dd8301ab016e9f2a6038e
hswick <hswick@smu.edu>
2016-10-06 00:02:56 -0500

    Translating room down and setting block constants

--- public/block.js
+++ public/block.js
@@ -1,20 +1,10 @@
-function cube(group, x, y, z) {
-	geometry = new THREE.BoxBufferGeometry( 10, 10, 10 );
-	geometry.translate(x, y, z);
-	//material = new THREE.MeshBasicMaterial({map: texture});
-	material = new THREE.MeshPhongMaterial({color: new THREE.Color(1, 0, 0)});
-	mesh = new THREE.Mesh( geometry, material );
-	mesh.castShadow = true;
-	group.add( mesh );
-}
-
 var counter = 0;
 
 var pastels = ["#e1f7d5", "#ffbdbd", "#c9c9ff", "#ffffff", "#f1cbff"];
 
 function cursorBlock(x, y, z) {
-	geometry = new THREE.BoxBufferGeometry( 30, 20, 30 );
-	geometry.translate(x, y+20, z);
+	geometry = new THREE.BoxBufferGeometry( blockXSize, blockYSize, blockZSize );
+	geometry.translate(x, y, z);
 
 	geometry.computeBoundingBox();
 	material = new THREE.MeshPhongMaterial({color: pastels[counter++]});
@@ -27,12 +17,19 @@ function cursorBlock(x, y, z) {
 	return mesh;
 }
 
+function computeCursorBoundingBox() {
+	boxGeometry = new THREE.BoxBufferGeometry( blockXSize, blockYSize, blockZSize );
+	boxGeometry.translate(cursor.position.x, cursor.position.y, cursor.position.z);
+	boxGeometry.computeBoundingBox();
+	cursor.bounds = boxGeometry.boundingBox.clone();	
+}
+
 //This should copy a blocks information. vector3 as input
 function cursorWireframe(x, y, z) {
 	//console.log("wire being called");
 	wireframe = new THREE.Object3D();
-	boxGeometry = new THREE.BoxBufferGeometry( 30, 20, 30 );
-	boxGeometry.translate(x, y+20, z);
+	boxGeometry = new THREE.BoxBufferGeometry( blockXSize, blockYSize, blockZSize );
+	boxGeometry.translate(x, y, z);
 	boxGeometry.computeBoundingBox();
 	bounds = boxGeometry.boundingBox.clone();
 
@@ -53,17 +50,17 @@ function cursorWireframe(x, y, z) {
 	topWire.add(l0, l1, l2, l3);
 
 	bottomWire = topWire.clone(true);
-	bottomWire.translateY(-10);//-y/2
+	bottomWire.translateY(-blockYSize/2);//-y/2
 
 	wireframe.add(topWire, bottomWire);
 	//wireframe.translateY(-5);//y/4
 
-	l4 = edge(new THREE.Vector3(x, y+20, z), new THREE.Vector3(x, w/2, z));
-	l5 = edge(new THREE.Vector3(x, y+20, z), new THREE.Vector3(w, y+20, z));
-	l6 = edge(new THREE.Vector3(x, y+20, z), new THREE.Vector3(-w, y+20, z));
+	l4 = edge(new THREE.Vector3(x, y, z), new THREE.Vector3(x, w/2, z));
+	l5 = edge(new THREE.Vector3(x, y, z), new THREE.Vector3(w, y, z));
+	l6 = edge(new THREE.Vector3(x, y, z), new THREE.Vector3(-w, y, z));
 	//l7 = edge(new THREE.Vector3(x, y+20, z), new THREE.Vector3(x, -w/2, z)); dont need bottom cursor cuz of falling motion
-	l8 = edge(new THREE.Vector3(x, y+20, z), new THREE.Vector3(x, y, h));
-	l9 = edge(new THREE.Vector3(x, y+20, z), new THREE.Vector3(x, y, -h));
+	l8 = edge(new THREE.Vector3(x, y, z), new THREE.Vector3(x, y, h));
+	l9 = edge(new THREE.Vector3(x, y, z), new THREE.Vector3(x, y, -h));
 	wireframe.add(l4);
 
 	wireframe.bounds = bounds;
@@ -85,7 +82,9 @@ function cursorCollision(cursor, block) {
 	bounds1.min = min;
 	bounds1.max = max;
 	bounds2 = block.geometry.boundingBox;
-	return min.distanceTo(bounds2.min) + max.distanceTo(bounds2.max) == 0.0;
+	dist = min.distanceTo(bounds2.min) + max.distanceTo(bounds2.max)
+	//console.log(dist);
+	return dist == 0.0;
 }
 
 function blockUnderneath(cursor, block) {
@@ -97,8 +96,8 @@ function blockUnderneath(cursor, block) {
 	bounds1.max = max;
 	bounds2 = block.geometry.boundingBox;
 
-	bottomMin = new THREE.Vector3().addVectors(min, new THREE.Vector3(0, -20, 0));
-	bottomMax = new THREE.Vector3().addVectors(max, new THREE.Vector3(0, -20, 0));
+	bottomMin = new THREE.Vector3().addVectors(min, new THREE.Vector3(0, 0, 0));
+	bottomMax = new THREE.Vector3().addVectors(max, new THREE.Vector3(0, 0, 0));
 	return bottomMin.distanceTo(bounds2.min) + bottomMax.distanceTo(bounds2.max) == 0.0;
 
 	//console.log(block.position);
